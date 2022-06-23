95e83646840f97a15db5a299128db6b6a2b14545
Zoe Douglas <zoedouglas23@gmail.com>
2022-06-23 13:20:20 -0600

    Parsing select time-series

--- mysci.py
+++ mysci.py
@@ -1,5 +1,7 @@
 # Initialize my data variable
-data = []
+data = {'date':[],
+        'time':[],
+        'tempout':[]} # list[0] vs dict['key'] --> data['tempout']
 
 # Read the data file
 filename = "data/wxobs20170821.txt"
@@ -7,9 +9,10 @@ filename = "data/wxobs20170821.txt"
 with open(filename, "r") as datafile:
     for _ in range(3):
         datafile.readline()
-        print(_)
     
     for line in datafile:
         datum = line.split()
-        data.append(datum)
+        data['date'].append(datum[0])
+        data['time'].append(datum[1])
+        data['tempout'].append(datum[2])
 
